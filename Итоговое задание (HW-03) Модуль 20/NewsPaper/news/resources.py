a8af4389f595162f9d0cb014cd4185448d6d2ff0
PavlovAA8 <43086343+PavlovAA8@users.noreply.github.com>
2025-08-02 10:57:47 +0300

    Delete Итоговое задание (HW-03) Модуль 20/NewsPaper directory
    Ошибка загрузки

--- Итоговое задание (HW-03) Модуль 20/NewsPaper/news/resources.py
+++
@@ -1,6 +0,0 @@
-ARTICLE = 'AR'
-NEWS = 'NW'
-POST_TYPES = [
-    (ARTICLE, 'Статья'),
-    (NEWS, 'Новость'),
-]
--- Итоговое задание (HW-03) Модуль 20/NewsPaper/yandex_provider/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class YandexProviderConfig(AppConfig):
-    name = 'yandex_provider'
-
-    def ready(self):
-        from allauth.socialaccount import providers
-        from .provider import YandexProvider
-        providers.registry.register(YandexProvider)
